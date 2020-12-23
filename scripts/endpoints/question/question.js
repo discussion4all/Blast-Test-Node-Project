0afadb982dcf6ff3910d1496128a0c763528a020
Discussion4all <discussion4all@yahoo.com>
2020-12-23 12:38:17 +0530

    set other api

--- scripts/endpoints/question/question.js
+++ scripts/endpoints/question/question.js
@@ -11,4 +11,15 @@ module.exports.getAllQuestion = function(req,res){
     })
 }
 
+//get one question set in question table
+module.exports.getOneQuestionSet = function(req,res){
+    
+    DB.find(table,{'QuestionSet':Number(req.body.set)},function(err,result){
+        if(err){
+            res.send({msg:"Not Found",err:err,data:null});
+        }else{
+            res.send({msg:"Record Found",err:null,data:result});
+        }
+    })
+}
 
--- scripts/endpoints/question/questionRoutes.js
+++ scripts/endpoints/question/questionRoutes.js
@@ -5,5 +5,6 @@ const auth = require("../../middleware/auth");
 const questionController = require('./question');
 
 router.get("/getAllQuestion",auth,questionController.getAllQuestion);
+router.get("/getOneQuestionSet",auth,questionController.getOneQuestionSet);
 
 module.exports = router;
